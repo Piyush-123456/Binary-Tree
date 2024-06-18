ce79cdf89e697c75bdc0f56c61022abf8d5e5c52
Piyush-123456 <134592487+Piyush-123456@users.noreply.github.com>
2024-06-18 18:57:57 +0530

    Create Path Sum III.cpp

---
+++ Path Sum III.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int ans = 0;
+    void solve(TreeNode* root, long long sum) {
+        if(root==NULL){
+            return;
+        }
+        if(sum==root->val){
+            ans++;
+        }
+        solve(root->left, sum - root->val);
+        solve(root->right, sum- root->val);
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if (root) {
+            solve(root, targetSum);
+            pathSum(root->left, targetSum);
+            pathSum(root->right, targetSum);
+        }
+        return ans;
+    }
+};
