f3a668f5e7e79396cc2502a1cae2b3e63b3667d6
Piyush-123456 <134592487+Piyush-123456@users.noreply.github.com>
2024-06-18 20:11:06 +0530

    Create Sum of nodes of longest blood line.cpp

---
+++ Sum of nodes of longest blood line.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    pair<int,int> height(Node*root){
+        if(!root){ return {0,0};}
+        auto  lh = height(root->left);
+        auto  rh = height(root->right);
+        int sum = root->data;
+        if(lh.second==rh.second){
+            sum+= lh.second> rh.second ? lh.second : rh.second;
+        }
+        else if(lh.first> rh.first){
+            sum+= lh.second;
+        }
+        else{
+            sum+=rh.second;
+        }
+        return {max(lh.first, rh.first)+1,sum};
+    }
+    int sumOfLongRootToLeafPath(Node *root)
+    {
+        //code here
+        auto h = height(root);
+        return h.second;
+    }
+};
